e6a8c65d8a6c0d8a3acf6a54e16e09b61cd5357e
ireneom3 <ireneom3github@gmail.com>
2015-05-06 01:42:42 +0100

    v0.2
    test app

---
+++ DublinEventsMap/app/src/main/java/com/dublin/threeasians/dublineventsmap/map.java
@@ -0,0 +1,49 @@
+package com.dublin.threeasians.dublineventsmap;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+
+public class map extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        loadCustomMap();
+    }
+
+    private void loadCustomMap()
+    {
+        WebView browser = (WebView) findViewById(R.id.webview);
+        browser.getSettings().setLoadsImagesAutomatically(true);
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.loadUrl("https://www.google.com/maps/d/viewer?mid=zdRKzgGPNUmk.kqdobSrDcLsE");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
